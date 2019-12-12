09014e40999f29340ce886fd5776663b7483ecc6
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-12 21:22:47 +0700

    Update tambah_user.php

--- pages/proses/tambah_user.php
+++ pages/proses/tambah_user.php
@@ -5,9 +5,21 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $level 	 =	$_POST['level'];
 
-$query = mysqli_query($connect, "INSERT INTO admin VALUES (NULL, '$username', '$password', '$level');") or die(mysqli_connect_error());
- 
-if ($query) {
-    header('location:../user.php');
-}
+		$result=mysqli_query($connect,"SELECT username FROM admin WHERE username='$username'");
+		$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+		if(mysqli_num_rows($result) == 1)
+		{
+			$pesan = "exist";
+			header('location:../user.php?pesan=exist');
+		}
+		else
+		{
+			
+		$query = mysqli_query($connect, "INSERT INTO admin VALUES (NULL, '$username', '$password', '$level');") or die(mysqli_connect_error());
+			if($query)
+			{
+				 header('location:../user.php?pesan=sukses');
+			}
+		}
+
 ?>
