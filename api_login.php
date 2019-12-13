d0b4d50b1b205076eabd6a2e1dbc827b6b69f03d
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-13 10:27:28 +0700

    Update api_login.php

--- api_login.php
+++ api_login.php
@@ -1,32 +1,32 @@
 <?php
 	include_once "config.php";
 
-	 class usr{}
+	 // class usr{}
 	
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 	
-	/*if ((empty($username)) || (empty($password))) { 
-	$response = new usr();
+	if ((empty($username)) || (empty($password))) { 
+	// $response = new usr();
 	$response->success = 0;
 	$response->message = "Kolom tidak boleh kosong"; 
 	 	die(json_encode($response));
-	 }*/
+	 }
 	
 	 $query = mysqli_query($connect, "SELECT * FROM admin WHERE username='$username' AND password='$password'");
 	
 	 $row = mysqli_fetch_array($query);
 	
 	 if (!empty($row)){
-	 	$response = new usr();
+	 	// $response = new usr();
 	 	$response->success = 1;
 	 	$response->message = "Selamat datang ".$row['username'];
-	 	//$response->id_admin = $row['id_admin'];
+	 	$response->id_admin = $row['id_admin'];
 	 	$response->username = $row['username'];
 	 	die(json_encode($response));
 		
 	 } else { 
-	 	$response = new usr();
+	 	// $response = new usr();
 	 	$response->success = 2;
 	 	$response->message = "Username atau password salah";
 	 	die(json_encode($response));
