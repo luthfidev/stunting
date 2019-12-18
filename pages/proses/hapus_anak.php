5c238d47102b972bbc644562c46ff8c5552c493d
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-18 20:40:53 +0700

    Update hapus_anak.php

--- pages/proses/hapus_anak.php
+++ pages/proses/hapus_anak.php
@@ -1,11 +1,27 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
 <?php
 include('../../config.php');
  
 $id = $_GET['id_anak'];
  
-$query = mysqli_query($connect,"delete from anak where id_anak='$id'") or die(mysql_error());
+$query = mysqli_query($connect,"delete from anak where id_anak='$id'") or die(mysqli_connect_error());
  
 if ($query) {
-    header("location:../anak.php");
+    echo "
+ <script type='text/javascript'>
+  setTimeout(function () {  
+    Swal.fire({
+    title: 'Berhasil Di Hapus',
+    type: 'success',
+    timer: 3000,
+    showConfirmButton: true
+   });  
+  },10); 
+  window.setTimeout(function(){ 
+   window.location.replace('../anak.php');
+  },1000); 
+ </script>"; 
+    //header("location:../anak.php");
 }
 ?>
