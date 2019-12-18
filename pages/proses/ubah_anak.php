fa765f5c43d38de67f120370e8cee852b2ce2963
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-18 20:40:56 +0700

    Update ubah_anak.php

--- pages/proses/ubah_anak.php
+++ pages/proses/ubah_anak.php
@@ -1,15 +1,33 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
 <?php
 include '../../config.php';
- 
-$id = $_POST['id'];
-$nama = $_POST['nama'];
-$ibu = $_POST['ibu'];
-$umur = $_POST['umur'];
-$keterangan = $_POST['keterangan'];
 
-$query = mysql_query("UPDATE anak SET `nama_anak` = '$nama', `nama_ibu` = '$ibu', `umur` = '$umur', `keterangan` = '$keterangan' WHERE `id_anak` = '$id';") or die(mysql_error());
+
+$id = $_POST['id_anak'];
+$nama_anak = $_POST['nama_anak'];
+$jk = $_POST['jk'];
+$nama_ibu = $_POST['nama_ibu'];
+$umur = $_POST['umur'];
+
+$query = mysqli_query($connect, "UPDATE anak SET nama_anak = '$nama_anak', jk = '$jk', nama_ibu = '$nama_ibu', umur = '$umur' WHERE id_anak = '$id'") or die(mysqli_connect_error());
  
 if ($query) {
-    header('location:../anak.php');
+    echo "
+ <script type='text/javascript'>
+  setTimeout(function () {  
+    Swal.fire({
+    title: 'Berhasil Di ubah',
+    type: 'success',
+    timer: 3000,
+    showConfirmButton: true
+   });  
+  },10); 
+  window.setTimeout(function(){ 
+   window.location.replace('../anak.php');
+  } ,1000); 
+ </script>"; 
+    //header('location:../anak.php');
 }
 ?>
+
