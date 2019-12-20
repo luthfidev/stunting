a3b57db6b09bdc2f898e3f095edd474d4fa699a5
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-20 09:43:45 +0700

    Update hapus_user.php

--- pages/proses/hapus_user.php
+++ pages/proses/hapus_user.php
@@ -1,11 +1,27 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.css"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
 <?php
 include('../../config.php');
  
-$id = $_GET['id'];
+$id = $_GET['id_admin'];
  
-$query = mysql_query("delete from user where id_user='$id'") or die(mysql_error());
+$query = mysqli_query($connect,"delete from admin where id_admin='$id'") or die(mysqli_connect_error());
  
 if ($query) {
-    header("location:../user.php");
+    echo "
+ <script type='text/javascript'>
+  setTimeout(function () {  
+    Swal.fire({
+    title: 'Berhasil Di Hapus',
+    type: 'success',
+    timer: 3000,
+    showConfirmButton: true
+   });  
+  },10); 
+  window.setTimeout(function(){ 
+   window.location.replace('../user.php');
+  },1000); 
+ </script>"; 
+    //header("location:../anak.php");
 }
 ?>
