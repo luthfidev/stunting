ce4e4d4dca2bbc1d2bfa25d2a278b31b09187742
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2019-12-23 11:12:35 +0700

    update 2

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 ini_set('date.timezone', 'Asia/Jakarta');
 //host yang digunakan
 //99,9% tidak perlu dirubah
@@ -21,29 +21,5 @@ $connect = mysqli_connect($host, $user, $pass, $dbname) or die(mysqli_connect_er
  
 //memilih database yang akan digunakan
 //$dbselect = mysql_select_db($dbname);
-?>
+?> 
 
-<!-- <?php
-ini_set('date.timezone', 'Asia/Jakarta');
-//host yang digunakan
-//99,9% tidak perlu dirubah
-$host = 'localhost'; 
- 
-//username untuk login ke host
-//biasanya didapatkan pada email konfirmasi order hosting
-$user = 'ghea'; 
- 
-//jika menggunakan PC sendiri sebagai host,
-//secara default password dikosongkan
-$pass = '12345';
- 
-//isikan nama database sesuai database
-//yang dibuat pada langkah-1
-$dbname = 'db-ghea';
- 
-//mengubung ke host
-$connect = mysqli_connect($host, $user, $pass, $dbname) or die(mysqli_connect_error());
- 
-//memilih database yang akan digunakan
-//$dbselect = mysql_select_db($dbname);
-?> -->
