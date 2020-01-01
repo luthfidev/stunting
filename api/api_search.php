4d5d3369881e8262c27a57a4fc936ba6d8e8e2d0
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-01 22:28:16 +0700

    update search

--- api/api_search.php
+++ api/api_search.php
@@ -2,11 +2,13 @@
 require_once('../config.php');
 if($_SERVER['REQUEST_METHOD']=='POST') {
 
-  $nama_anak = $_POST['nama_anak'];
-  $nama_ibu = $_POST['nama_ibu'];
+
+  $no_medis = $_POST['no_medis'];
+ /*  $nama_anak = $_POST['nama_anak'];
+  $nama_ibu = $_POST['nama_ibu']; */
   // $tanggal = $_POST['tanggal'];
 
-  $res = mysqli_query($connect,"SELECT * FROM anak where nama_anak LIKE '%$nama_anak%' AND nama_ibu LIKE '%$nama_ibu%' ");
+  $res = mysqli_query($connect,"SELECT * FROM anak where no_medis LIKE '%$no_medis%' ");
  $cek = mysqli_num_rows($res);
   if ($cek == 0 ){
     $response->status = 0;
@@ -18,14 +20,15 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     $data = array();
     while($row = mysqli_fetch_array($res)){
       array_push($data, array('id_anak'=>$row[0], 
-      							'nama_anak'=>$row[1], 
-      							'jk' => $row[2],
-      							'nama_ibu'=>$row[3], 
-      							'umur'=>$row[4],
-      							'berat'=>$row[5],
-      							'tinggi'=>$row[6],
-      							'keterangan'=>$row[7],
-      							'tanggal'=>$row[8])
+                    'no_medis'=>$row[1], 
+      							'nama_anak'=>$row[2], 
+      							'jk' => $row[3],
+      							'nama_ibu'=>$row[4], 
+      							'umur'=>$row[5],
+      							'berat'=>$row[6],
+      							'tinggi'=>$row[7],
+      							'keterangan'=>$row[8],
+      							'tanggal'=>$row[9])
   									);
     }
     echo json_encode(array("status"=>1,"data"=>$data));
