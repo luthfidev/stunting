0336b06014d122088e4c3722be182716eef9869e
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-02 09:20:46 +0700

    update insert

--- api/api_insert.php
+++ api/api_insert.php
@@ -7,12 +7,13 @@
 	 $nama_ibu = $_POST["nama_ibu"];
 	 $jk = $_POST["jk"];
 	 $tanggal_lahir = $_POST["tanggal_lahir"];
+	 $umur = $_POST["umur"];
 	 $berat = $_POST["berat"];
 	 $tinggi = $_POST["tinggi"];
 	 $keterangan = $_POST["keterangan"];
 	 $tanggal = date("Y-m-d");
 	 
-		 $query = mysqli_query($connect, "INSERT INTO anak (id_anak, nama_anak, jk, nama_ibu, tanggal_lahir, berat, tinggi, keterangan, tanggal) VALUES(0,'".$nama_anak."','".$jk."','".$nama_ibu."','".$tanggal_lahir."','".$berat."','".$tinggi."','".$keterangan."','".$tanggal."')");
+		 $query = mysqli_query($connect, "INSERT INTO anak (id_anak, nama_anak, jk, nama_ibu, umur, tanggal_lahir, berat, tinggi, keterangan, tanggal) VALUES(0,'".$nama_anak."','".$jk."','".$nama_ibu."','".$umur."','".$tanggal_lahir."','".$berat."','".$tinggi."','".$keterangan."','".$tanggal."')");
 
 		 		if ($query){
 		 			$response = new usr();
