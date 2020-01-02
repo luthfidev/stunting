039606b02e827510e1da3ea219c811c8b90d1305
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-02 18:37:38 +0700

    Update detail_grafik.php

--- pages/detail_grafik.php
+++ pages/detail_grafik.php
@@ -1,7 +1,7 @@
 <?php 
   session_start();
   include '../config.php';
-
+    
   if (isset($_SESSION['username'])) {
   $username = $_SESSION['username'];
   $isLoggedIn = $_SESSION['isLoggedIn'];
@@ -10,7 +10,27 @@
   else {
     header('location:../index.php?pesan=belum_login');
   }
-?><html>
+   $nama_anak = null;
+
+  if(isset($_GET["nama_anak"]))
+  {
+    $nama_anak = $_GET["nama_anak"];
+  }
+  $query = mysqli_query($connect, "select tanggal, nama_anak,
+  COUNT(case when anak.keterangan='Stunting Gizi Baik' then 1 end) as baik,
+  COUNT(case when anak.keterangan='Stunting Gizi Buruk' then 1 end) as buruk,
+  COUNT(case when anak.keterangan='Stunting Gizi Kurang' then 1 end) as kurang,
+  COUNT(case when anak.keterangan='Gizi lebih' then 1 end) as lebih 
+  from anak where nama_anak = '".$nama_anak."'  ");
+  $chart_data = '';
+  while($row = mysqli_fetch_array($query))
+  {
+    $chart_data .= "{ tanggal: '".$row["tanggal"]."', baik: ".$row["baik"].", buruk: ".$row["buruk"].", lebih: ".$row["lebih"].", kurang: ".$row["kurang"].",}, ";
+  }
+  $chart_data = substr($chart_data, 0, -2);
+
+?>
+<html>
 
 <head>
   <meta charset="utf-8">
@@ -28,7 +48,7 @@
   <!-- Argon CSS -->
   <link type="text/css" href="../assets/css/argon.css?v=1.0.0" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="../assets/DataTables/datatables.min.css"/>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 </head>
 
 
@@ -55,26 +75,35 @@
           <div class="card shadow">
             <div class="card-header border-0">
               <div class="row align-items-center">
-                <div class="col">
-                  <h1 class="mb-0">Detail Data Anak</h1>
-                </div>
-                <div class="col text-right">
-                  <!-- <a href="report/cetak_report_rekap_stok.php" class="btn btn-danger">Cetak Report</a> -->
-                   <!-- <a href="#" data-toggle="modal" data-target="#AddModal" class="btn btn-success">Tambah</a> --> 
+              <div class="container-fluid">
+                  <div class="header-body">
+                    <!-- Card stats -->
+                    <div class="row">
+                
+
+                      <div class="col-xl-12 col-lg-6">
+                        <div class="card-body">
+                           <div class="card card-stats mb-4 mb-xl-0">
+                              <div id="chart"></div>
+                            </div>
+                        </div>
+                      </div>
+
+                  </div>
                 </div>
               </div>
             </div>
-           
+          </div>
+        </div>
+      </div>
 
-   
+    </div>
+    
 
-   
 
-   
   <!-- Argon Scripts -->
   <!-- Core -->
   <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.min.js"></script>
   <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <!-- Optional JS -->
   <script src="../assets/vendor/chart.js/dist/Chart.min.js"></script>
@@ -82,72 +111,46 @@
   <!-- Argon JS -->
   <script src="../assets/js/argon.js?v=1.0.0"></script>
   <script type="text/javascript" src="../assets/DataTables/datatables.min.js"></script>
-  <script type="text/javascript">
-    $(document).ready(function() {
-      $('#data').DataTable( {        
-        "paging": true,     
-        "ordering": false,     
-        "info": true,     
-        "searching": true,     
-        "pagingType": "numbers",
-        "bLengthChange" : false
-      }
-      );
-    });
-  </script>
-  <script type="text/javascript">
-    $(".form_datetime").datetimepicker({
-        format: "dd MM yyyy - hh:ii"
-    });
-</script>       
 
-  <script type="text/javascript">
-          $('#EditModal').on('show.bs.modal', function (event) {
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var recipient = button.data('what0')
-            var recipient1 = button.data('what1')
-            var recipient2 = button.data('what2')
-            var recipient3 = button.data('what3')
-            var recipient4 = button.data('what4')
-             // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this)
-            modal.find('.modal-body #id').val(recipient)
-            modal.find('.modal-body #nama').val(recipient1)
-            modal.find('.modal-body #keterangan').val(recipient3)
-            modal.find('.modal-body #umur').val(recipient4)
-            modal.find('.modal-body #ibu').val(recipient2)
-          })
-  </script>
-  <script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+ <!-- <script src="../assets/js/grafik_gizi.js"></script>   -->
 
-$('.del').on('click', function (e){
-  e.preventDefault();
-  const href = $(this).attr('href')
-  Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.value) {
-        document.location.href = href;
-      }
-    })
-})
+<script>
+/* new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'chart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [<?php echo $chart_data; ?> ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'tanggal',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['baik','buruk'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['baik','buruk']
+}); */
 
-  $('.del-tbn').on('click', function(){
-        swall.fire({
-          type : 'success',
-          tittle: 'Tittla',
-          text: 'text'
-        })
-})
-  </script>
+new Morris.Area({
+  // ID of the element in which to draw the chart.
+  element: 'chart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [<?php echo $chart_data; ?>  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'tanggal',
+  parseTime: false,
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['baik','buruk','lebih','kurang'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Baik','Buruk','Lebih','kurang'],
+  lineColors: ['#373651','#E65A26','#0bb356','#ebe834']
+});
+</script>
+
 </body>
 
 </html>
