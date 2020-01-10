7010e59e8daa26e66f5e7a4f8a8a9a2d38d46b33
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-10 09:07:05 +0700

    update insert ortu

--- api/api_insert_ortu.php
+++ api/api_insert_ortu.php
@@ -7,7 +7,7 @@
 	 $nama_ibu 		= $_POST["nama_ibu"];
 	 $alamat 		= $_POST["alamat"];
 	 
-		 $query = mysqli_query($connect, "INSERT INTO ortu (id_anak, nama_ayah, nama_ibu, alamat) VALUES(0,'".$nama_ayah."','".$nama_ibu."','".$alamat."')");
+		 $query = mysqli_query($connect, "INSERT INTO ortu (id_ortu, nama_ayah, nama_ibu, alamat) VALUES(0,'".$nama_ayah."','".$nama_ibu."','".$alamat."')");
 
 		 		if ($query){
 		 			$response = new usr();
