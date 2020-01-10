1617b1e3abe4bbdec5c98a336f3b683064d318b2
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-10 09:00:31 +0700

    add insert ortu

---
+++ api/api_insert_ortu.php
@@ -0,0 +1,30 @@
+<?php
+	include_once "../config.php";
+
+	 class usr{}
+
+     $nama_ayah 	= $_POST["nama_ayah"];
+	 $nama_ibu 		= $_POST["nama_ibu"];
+	 $alamat 		= $_POST["alamat"];
+	 
+		 $query = mysqli_query($connect, "INSERT INTO ortu (id_anak, nama_ayah, nama_ibu, alamat) VALUES(0,'".$nama_ayah."','".$nama_ibu."','".$alamat."')");
+
+		 		if ($query){
+		 			$response = new usr();
+		 			$response->status = 1;
+		 			$response->messages = "berhasil Simpan.";
+		 			die(json_encode($response));
+
+		 		} else {
+		 			$response = new usr();
+		 			$response->status = 0;
+		 			$response->messages = "Gagal";
+		 			die(json_encode($response));
+		 		}
+		 	
+		 
+	 
+
+	 mysqli_close($connect);
+
+?>	
