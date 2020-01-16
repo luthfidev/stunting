e9cb74c5658a56323177464b32c5c98b209047b0
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-16 11:07:37 +0700

    update 2

--- api/api_nomedis.php
+++ api/api_nomedis.php
@@ -6,12 +6,12 @@ require_once('../config.php');
   $nama_ibu = $_POST['nama_ibu']; */
   // $tanggal = $_POST['tanggal'];
 
-$res = mysqli_query($connect,"SELECT * FROM anak order by no_medis");
+$res = mysqli_query($connect,"SELECT * FROM anak order by id_anak");
  $cek = mysqli_num_rows($res);
 if ($cek > 0){
     $data = array();
     while($row = mysqli_fetch_array($res)){
-      array_push($data, array('no_medis'=>$row[1])
+      array_push($data, array('id_anak'=>$row[0])
                 );
     }
     echo json_encode(array("message"=>1,"semuamedis"=>$data));
