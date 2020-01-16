30aeb4896582761e9088c5f97e45b8c1d3b2f5cb
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-16 12:01:21 +0700

    update 2

--- api/api_insert_lama.php
+++ api/api_insert_lama.php
@@ -3,10 +3,9 @@
 
 	 class usr{}
 
-	 $no_medis  	= $_POST["no_medisanak"];
+     $no_medis  	= $_POST["no_medisanak"];
+     $umur 			= $_POST["usia"];
 	 $jk 			= $_POST["jenis_kelamin"];
-	// $tanggal_lahir = $_POST["tanggal_lahir"];
-	 $umur 			= $_POST["usia"];
 	 $berat 		= $_POST["bb_anak"];
 	 $tinggi 		= $_POST["tb_anak"];
 	 //$keterangan 	= $_POST["keterangan"];
@@ -14,7 +13,7 @@
 	 $status_gizi 	= $_POST["status_gizi"];
 	 $tanggal		= date("Y-m-d");
 	 
-		 $query = mysqli_query($connect, "INSERT INTO pengukuran (id_pengukuran, no_medisanak, jenis_kelamin, usia, bb_anak, tb_anak, status_stunting, status_gizi, tanggal_pengukuran) VALUES(0,'".$no_medis."','".$jk."','".$umur."','".$tanggal_lahir."','".$berat."','".$tinggi."','".$status_stunting."','".$status_gizi."','".$tanggal."')");
+		 $query = mysqli_query($connect, "INSERT INTO pengukuran (id_pengukuran, no_medisanak, usia, jenis_kelamin,  bb_anak, tb_anak, status_stunting, status_gizi, tanggal_pengukuran) VALUES(0,'".$no_medis."','".$umur."','".$jk."','".$berat."','".$tinggi."','".$status_stunting."','".$status_gizi."','".$tanggal."')");
 
 		 		if ($query){
 		 			$response = new usr();
