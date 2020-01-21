6903a405e000337b4e3f3fd7449051531314c96a
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-21 18:48:10 +0700

    update4

--- api/api_insert.php
+++ api/api_insert.php
@@ -2,12 +2,13 @@
 	include_once "../config.php";
 
 	 class usr{}
-
+	$nik_anak = $_POST["nik_anak"];
 	 $nama_anak 	= $_POST["nama_anak"];
 
 	 /* $nama_ayah 	= $_POST["nama_ayah"];
 	 $alamat 		= $_POST["alamat"]; */
 	 $jk 			= $_POST["jk"];
+	 $tempat_lhr		= $_POST["tempat_lhr"];
 	 $tanggal_lahir = $_POST["tanggal_lahir"];
 	 $umur 			= $_POST["umur"];
 	 $berat 		= $_POST["berat"];
@@ -17,7 +18,7 @@
 	 $status_gizi 	= $_POST["status_gizi"];
 	 $tanggal		= date("Y-m-d");
 	 
-		 $query = mysqli_query($connect, "INSERT INTO temp_anak (id_anak, nama_anak, jk, umur, tanggal_lahir, berat, tinggi, status_stunting, status_gizi, tanggal) VALUES(0,'".$nama_anak."','".$jk."','".$umur."','".$tanggal_lahir."','".$berat."','".$tinggi."','".$status_stunting."','".$status_gizi."','".$tanggal."')");
+		 $query = mysqli_query($connect, "INSERT INTO temp_anak (id_anak, nik_anak, nama_anak, jk, umur, tempat_lhr,tanggal_lahir, berat, tinggi, status_stunting, status_gizi, tanggal) VALUES(0,'".$nik_anak."','".$nama_anak."','".$jk."','".$umur."','".$tempat_lhr."','".$tanggal_lahir."','".$berat."','".$tinggi."','".$status_stunting."','".$status_gizi."','".$tanggal."')");
 	
 		 		if ($query){
 		 			$response = new usr();
