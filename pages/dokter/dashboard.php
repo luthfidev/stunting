8dcc43157a016b4d0557e86005f773fb25fb90b8
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-01-30 08:57:36 +0700

    revisi 30012020 0857

--- pages/dokter/dashboard.php
+++ pages/dokter/dashboard.php
@@ -142,7 +142,7 @@
                                 <h5 class="card-title text-uppercase text-muted mb-0">Sehat</h5>
                                 <span class="h2 font-weight-bold mb-0">
                                 <?php
-                                $result=mysqli_query($connect, "SELECT count(*) as sehat from pengukuran where status_gizi = 'Gizi Baik' ");
+                                $result=mysqli_query($connect, "SELECT count(*) as sehat from pengukuran where status_stunting = 'Normal' GROUP BY no_medisanak ");
                                 $data=mysqli_fetch_assoc($result);
                                 echo $data['sehat'];
                                 ?>
@@ -166,7 +166,7 @@
                                 <h5 class="card-title text-uppercase text-muted mb-0">Tidak Sehat</h5>
                                 <span class="h2 font-weight-bold mb-0">
                                 <?php
-                                $result=mysqli_query($connect, "SELECT count(*) as tsehat from pengukuran where status_gizi = 'Gizi Buruk' ");
+                                $result=mysqli_query($connect, "SELECT count(*) as tsehat from pengukuran where status_stunting = 'Stunting' GROUP BY no_medisanak ");
                                 $data=mysqli_fetch_assoc($result);
                                 echo $data['tsehat'];
                                 ?>
